16350af100b5f1977148823e307d4dd4d51b6332
eL7Alfa <90084686+eL7Alfa@users.noreply.github.com>
2021-10-14 19:05:16 +0800

    Code clean up, reformat

--- src/components/Auth/styles.ts
+++ src/components/Auth/styles.ts
@@ -1,5 +1,6 @@
 import { createStyles, makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material/styles';
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
