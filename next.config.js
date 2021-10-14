a436abe12dcbb8ad99a327a1b789df67c7e623a2
eL7Alfa <90084686+eL7Alfa@users.noreply.github.com>
2021-10-14 12:31:38 +0800

    Add new host for images.domains, remove temp host

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withImages = require('next-images');
 module.exports = withImages({
   reactStrictMode: false,
   images: {
-    domains: ['kbi.sfo3.digitaloceanspaces.com', 'demos.creative-tim.com'],
+    domains: ['kbi.sfo3.digitaloceanspaces.com', 'cdn.kerbel.in'],
   },
   webpack: (config, options) => {
     // Important: return the modified config
