a13f751901206890a320f70dfc92f5538d61ac4c
eL7Alfa <90084686+eL7Alfa@users.noreply.github.com>
2021-10-14 12:28:55 +0800

    Remove unused imports and rearrange code

--- src/components/Home/FeaturedServices/index.tsx
+++ src/components/Home/FeaturedServices/index.tsx
@@ -1,8 +1,6 @@
-import { Grid, Paper } from '@mui/material';
+import { Grid } from '@mui/material';
 import useStyles from './styles';
-import { AutoFixHigh, FlightOutlined } from '@mui/icons-material';
 import FeaturedItem, { FeaturedItemProps } from './FeaturedItem';
-import { JSXElementConstructor, Key, ReactElement } from 'react';
 
 type FeaturedServicesProps = {
   data: FeaturedItemProps[];
@@ -13,12 +11,7 @@ const FeaturedServices = ({ data }: FeaturedServicesProps) => {
   return (
     <Grid container spacing={2} my={2} px={2}>
       {data.map((d, key) => (
-        <Grid
-          key={key}
-          item
-          lg={3}
-          xs={6}
-          className={classes.gridItem}>
+        <Grid key={key} item lg={3} xs={6} className={classes.gridItem}>
           <FeaturedItem
             icon={d.icon}
             iconBgColor={d.iconBgColor}
