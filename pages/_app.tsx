9d9755e1a3188efbacd6d7768e0e5b8f4797ac22
eL7Alfa <ukasyahrz@outlook.com>
2021-10-14 13:05:01 +0800

    Add Redux Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@
 import type { AppProps } from 'next/app';
 import theme from '../src/config/theme';
 import { ThemeProvider } from '@mui/styles';
+import { Provider } from 'react-redux';
+import store from '../src/redux/reducers';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider {...{ theme }}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider {...{ store }}>
+      <ThemeProvider {...{ theme }}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
 export default MyApp;
