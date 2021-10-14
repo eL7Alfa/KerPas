c7967939a77efe95e1360390d9f1627afd752feb
eL7Alfa <90084686+eL7Alfa@users.noreply.github.com>
2021-10-14 12:21:29 +0800

    Fix searchbar focus animation

--- src/components/Home/AppHeader/index.tsx
+++ src/components/Home/AppHeader/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
-import { Button, ClickAwayListener, useTheme } from '@mui/material';
+import { Button, ClickAwayListener, Hidden, useTheme } from '@mui/material';
 import CartButton from '../../CartButton';
 import useStyles from './styles';
 
@@ -14,16 +14,19 @@ const AppHeader = () => {
   const [searchWidth, setSearchWidth] = useState('60%');
 
   const onSearchFocused = () => {
-    if (window.innerWidth > theme.breakpoints.values.lg) {
-      setSearchWidth(`${theme.breakpoints.values.lg - 16 * 2}px`);
-    } else {
-      setSearchWidth('100%');
-    }
+    setSearchWidth('100%');
   };
 
   const onSearchBlurred = () => {
     setSearchWidth('60%');
   };
+
+  useEffect(() => {
+    window.addEventListener('resize', onSearchBlurred);
+    return () => {
+      window.removeEventListener('resize', onSearchBlurred);
+    };
+  }, []);
 
   return (
     <AppBar position="fixed" elevation={5} className={classes.root}>
@@ -31,25 +34,29 @@ const AppHeader = () => {
         <Typography className={classes.title} variant="h6" noWrap>
           KERPAS
         </Typography>
-        <div className={classes.searchW}>
-          <ClickAwayListener onClickAway={onSearchBlurred}>
-            <div className={classes.search} style={{ width: `${searchWidth}` }}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
+        <Hidden smDown>
+          <div className={classes.searchW}>
+            <ClickAwayListener onClickAway={onSearchBlurred}>
+              <div
+                className={classes.search}
+                style={{ width: `${searchWidth}` }}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder="Cari di kerpas"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                  inputProps={{ 'aria-label': 'search' }}
+                  onFocus={onSearchFocused}
+                />
+                <Button className={classes.searchBtn}>Cari</Button>
               </div>
-              <InputBase
-                placeholder="Cari di kerpas"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-                onFocus={onSearchFocused}
-              />
-              <Button className={classes.searchBtn}>Cari</Button>
-            </div>
-          </ClickAwayListener>
-        </div>
+            </ClickAwayListener>
+          </div>
+        </Hidden>
         <div className={classes.toolbarItemRight}>
           <CartButton classes={{ iconButton: classes.cartBtn }} />
           <Button className={classes.loginButton} color={'secondary'}>
