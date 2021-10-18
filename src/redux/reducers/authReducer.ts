9aa33d994bcf60850526dc397c41e96028b39867
eL7Alfa <90084686+eL7Alfa@users.noreply.github.com>
2021-10-19 02:16:42 +0800

    Add setAuthResetUserDataT condition

--- src/redux/reducers/authReducer.ts
+++ src/redux/reducers/authReducer.ts
@@ -1,11 +1,21 @@
 import authDefState from '../defaultState/authDefState';
 import { AnyAction } from 'redux';
-import { setAuthModalOpenT } from '../Types/Auth';
+import {
+  setAuthModalOpenT,
+  setAuthResetUserDataT,
+  setAuthUserDataT,
+} from '../Types/Auth';
 
 const authReducer = (state = authDefState, { type, payload }: AnyAction) => {
   switch (type) {
     case setAuthModalOpenT: {
       return { ...state, modalOpen: payload };
+    }
+    case setAuthUserDataT: {
+      return { ...state, userData: payload };
+    }
+    case setAuthResetUserDataT: {
+      return { ...state, userData: payload };
     }
     default:
       return state;
