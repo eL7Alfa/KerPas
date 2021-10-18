c45235ef2a63a12d8fe293395fb8da5cd867ba82
eL7Alfa <90084686+eL7Alfa@users.noreply.github.com>
2021-10-18 14:36:18 +0800

    Add userData and its type definition

--- src/redux/defaultState/authDefState.ts
+++ src/redux/defaultState/authDefState.ts
@@ -1,9 +1,41 @@
 export type authDefStateT = {
   modalOpen: boolean;
+  userData: {
+    id: number;
+    ckode_user: string;
+    cfb_id: string | null;
+    cnama: string;
+    cemail: string;
+    ctelp: string;
+    fbs_token: string;
+    cprofile_picture: string;
+    mstatus_login: number;
+    cactive: string;
+    nsaldo_voucher: string;
+    dcreate: string;
+    dmodify: string;
+    token: string;
+  };
 };
 
 const authDefState: authDefStateT = {
   modalOpen: false,
+  userData: {
+    id: 0,
+    ckode_user: '',
+    cfb_id: null,
+    cnama: '',
+    cemail: '',
+    ctelp: '',
+    fbs_token: '',
+    cprofile_picture: '',
+    mstatus_login: 0,
+    cactive: '0',
+    nsaldo_voucher: '0',
+    dcreate: '',
+    dmodify: '',
+    token: '',
+  },
 };
 
 export default authDefState;
