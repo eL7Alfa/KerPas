5459198ba7e85318122d923c9a901e5ab1d1be8d
eL7Alfa <90084686+eL7Alfa@users.noreply.github.com>
2021-10-19 02:14:46 +0800

    Add setAuthUserDataR, setAuthResetUserDataT

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,8 +1,24 @@
 import { setAuthModalOpenT } from './Types/Auth';
+import { setAuthUserDataT } from './Types/Auth';
+import authDefState from './defaultState/authDefState';
 
 export const setAuthModalOpenR = (payload: boolean) => {
   return {
     type: setAuthModalOpenT,
     payload,
+  };
+};
+
+export const setAuthUserDataR = (payload: boolean) => {
+  return {
+    type: setAuthUserDataT,
+    payload,
+  };
+};
+
+export const setAuthResetUserDataT = () => {
+  return {
+    type: setAuthUserDataT,
+    payload: authDefState.userData,
   };
 };
