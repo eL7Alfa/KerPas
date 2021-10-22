d50e780a168afe4503de08e8d85c1406c66f47ad
eL7Alfa <90084686+eL7Alfa@users.noreply.github.com>
2021-10-22 15:54:05 +0800

    Change authDefState to authStateR

--- src/redux/reducers/authReducer.ts
+++ src/redux/reducers/authReducer.ts
@@ -1,4 +1,4 @@
-import authDefState from '../defaultState/authDefState';
+import authStateR from '../defaultStateR/authStateR';
 import { AnyAction } from 'redux';
 import {
   setAuthModalOpenT,
@@ -6,7 +6,7 @@ import {
   setAuthUserDataT,
 } from '../Types/Auth';
 
-const authReducer = (state = authDefState, { type, payload }: AnyAction) => {
+const authReducer = (state = authStateR, { type, payload }: AnyAction) => {
   switch (type) {
     case setAuthModalOpenT: {
       return { ...state, modalOpen: payload };
