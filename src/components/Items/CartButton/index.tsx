00952258b36e77f519cd6d7fcd496b9ed425c736
eL7Alfa <90084686+eL7Alfa@users.noreply.github.com>
2021-10-23 14:28:27 +0800

    Separate styles of CartButton

--- src/components/Items/CartButton/index.tsx
+++ src/components/Items/CartButton/index.tsx
@@ -10,9 +10,8 @@ import {
 } from '@mui/material';
 import { ShoppingCart } from '@mui/icons-material';
 import Typography from '@mui/material/Typography';
-import { createStyles, makeStyles } from '@mui/styles';
-import { Theme } from '@mui/material/styles';
 import clsx from 'clsx';
+import useStyles from './styles';
 
 type CartButtonPropsType = {
   classes?: { iconButton: string };
@@ -60,33 +59,5 @@ const CartButton = ({
     </Fragment>
   );
 };
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    cartButton: {
-      backgroundColor: theme.palette.background.default,
-      borderRadius: 8,
-      '&.MuiButton-root': {
-        boxShadow:
-          '0px 3px 1px -2px rgba(254,204,43,0.2), 0px 2px 2px 0px rgba(254,204,43,0.1), 0px 1px 5px 0px rgba(254,204,43,0.08)',
-        color: theme.palette.background.default,
-        '&:hover': {
-          boxShadow:
-            '0px 5px 10px 5px rgba(254,204,43,0.3), 0px 5px 2px 0px rgba(254,204,43,0.1), 0px 1px 5px 0px rgba(254,204,43,0.08)',
-          backgroundColor: theme.palette.secondary.main,
-        },
-      },
-    },
-    cartMenuPaperW: {
-      paddingTop: 10,
-    },
-    paperPopCartList: {
-      padding: '10px 0',
-      borderRadius: 10,
-      boxShadow:
-        '0 13px 27px -5px rgb(50 50 93 / 25%), 0 8px 16px -8px rgb(0 0 0 / 30%), 0 -6px 16px -6px rgb(0 0 0 / 3%)',
-    },
-  }),
-);
 
 export default CartButton;
---
+++ src/components/Items/CartButton/styles.ts
@@ -0,0 +1,32 @@
+import { createStyles, makeStyles } from '@mui/styles';
+import { Theme } from '@mui/material/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    cartButton: {
+      backgroundColor: '#ffffff',
+      borderRadius: 8,
+      '&.MuiButton-root': {
+        boxShadow:
+          '0px 3px 1px -2px rgba(254,204,43,0.2), 0px 2px 2px 0px rgba(254,204,43,0.1), 0px 1px 5px 0px rgba(254,204,43,0.08)',
+        color: theme.palette.background.default,
+        '&:hover': {
+          boxShadow:
+            '0px 5px 10px 5px rgba(254,204,43,0.3), 0px 5px 2px 0px rgba(254,204,43,0.1), 0px 1px 5px 0px rgba(254,204,43,0.08)',
+          backgroundColor: theme.palette.secondary.main,
+        },
+      },
+    },
+    cartMenuPaperW: {
+      paddingTop: 10,
+    },
+    paperPopCartList: {
+      padding: '10px 0',
+      borderRadius: 10,
+      boxShadow:
+        '0 13px 27px -5px rgb(50 50 93 / 25%), 0 8px 16px -8px rgb(0 0 0 / 30%), 0 -6px 16px -6px rgb(0 0 0 / 3%)',
+    },
+  }),
+);
+
+export default useStyles;
