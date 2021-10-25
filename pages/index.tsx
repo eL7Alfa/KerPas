3e59e0962c712c2443e99a045beeab4fd78754e4
eL7Alfa <90084686+eL7Alfa@users.noreply.github.com>
2021-10-26 02:50:01 +0800

    Change Box mt from 12 to 10

--- pages/index.tsx
+++ pages/index.tsx
@@ -234,7 +234,7 @@ export default function Home({
       <CssBaseline />
       <AppHeader />
       <Container maxWidth={'lg'} sx={{ px: '0!important', overflow: 'hidden' }}>
-        <Box py={1} mt={12} mb={8}>
+        <Box py={1} mt={10} mb={8}>
           <Campaign data={getCampaigns.result} />
           <FeaturedServices data={featuredServiceData} />
           <Categories data={menu} />
