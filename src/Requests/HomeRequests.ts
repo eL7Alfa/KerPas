f0173ff3d8d9f9f615351edaf0654f980681a380
elalfa <ukasyahrz@outlook.com>
2021-11-04 11:15:05 +0800

    Add if marketCode condition to getPromotedProducts

--- src/Requests/HomeRequests.ts
+++ src/Requests/HomeRequests.ts
@@ -165,24 +165,26 @@ export const useGetPromotedProducts = (marketCode: string) => {
 export const useGetProductsByCategory = (marketCode: string) => {
   const [productsByCategory, setProductsByCategory] = useState<any[]>([]);
   useEffect(() => {
-    axios()
-      .post('/market/product', {
-        limit: 12,
-        type: 'category',
-        subcategory: selectedCatId,
-        marketId: marketCode,
-      })
-      .then(({ data }) => {
-        if (
-          data.response === 200 &&
-          data &&
-          !data.error &&
-          data.result.data.length
-        ) {
-          setProductsByCategory(newProducts(data.result.data));
-        }
-      })
-      .catch(() => {});
+    if (marketCode) {
+      axios()
+        .post('/market/product', {
+          limit: 12,
+          type: 'category',
+          subcategory: selectedCatId,
+          marketId: marketCode,
+        })
+        .then(({ data }) => {
+          if (
+            data.response === 200 &&
+            data &&
+            !data.error &&
+            data.result.data.length
+          ) {
+            setProductsByCategory(newProducts(data.result.data));
+          }
+        })
+        .catch(() => {});
+    }
   }, [marketCode]);
   return { productsByCategory, setProductsByCategory };
 };
