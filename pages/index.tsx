b82341af00249a7f60354f82cd9046da57e4215a
elalfa <ukasyahrz@outlook.com>
2021-11-10 01:05:17 +0800

    Create useInit, move some baseInit from page/index to src/components/constants (useInit)

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,13 +9,13 @@ import Categories from '../src/components/Home/Categories';
 import Stores from '../src/components/Home/Stores';
 import Promo from '../src/components/Home/Promo';
 import Products from '../src/components/Home/Products';
-import { checkUserData, newProducts } from '../src/components/constants';
+import { newProducts, useInit } from '../src/components/constants';
 import Auth from '../src/components/Auth';
 import {
   featuredServiceData,
   selectedCatId,
 } from '../src/components/Home/constants';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { marketImgUrl } from '../src/config/urls';
 import ProductsByCategory from '../src/components/Home/ProductsByCategory';
 import NearestMarket from '../src/components/Home/NearestMarket';
@@ -26,14 +26,11 @@ import {
   useGetPromotedProducts,
   useGetSupplier,
 } from '../src/Requests/HomeRequests';
-import { setAuthUserDataR } from '../src/redux/actions/authRActions';
 import { rootReducerI } from '../src/redux/reducers';
 import { useGetProductsByCategory } from '../src/Requests/GlobalRequests';
-import { setNearestMarketR } from '../src/redux/actions/appRActions';
 
 export default function Home() {
   const selector = useSelector((state: rootReducerI) => state);
-  const dispatch = useDispatch();
   const [nearestMarket, setNearestMarket] = useState<{ [key: string]: any }>(
     {},
   );
@@ -81,69 +78,11 @@ export default function Home() {
     }
   };
 
-  let locationRequestCount = 0;
-  const getNearestMarketByDeviceAddress = () => {
-    locationRequestCount++;
-    navigator.geolocation.getCurrentPosition(
-      ({ coords }) => {
-        const { latitude, longitude, accuracy } = coords;
-        if (accuracy <= 50) {
-          axios()
-            .post('/market/nearest', { lat: latitude, lng: longitude })
-            .then(({ data: { result, response } }) => {
-              if (response === 200) {
-                dispatch(setNearestMarketR(result));
-              }
-            });
-        } else {
-          if (locationRequestCount <= 6) {
-            getNearestMarketByDeviceAddress();
-          }
-        }
-      },
-      () => {
-        if (locationRequestCount <= 6) {
-          getNearestMarketByDeviceAddress();
-        }
-      },
-      { enableHighAccuracy: true, timeout: 3000 },
-    );
-  };
-
-  const getNearestMarket = ({ lat, lng }: { lat: number; lng: number }) => {
-    axios()
-      .post('/market/nearest', { lat, lng })
-      .then(({ data: { result, response } }) => {
-        if (response === 200) {
-          dispatch(setNearestMarketR(result));
-        } else {
-          dispatch(setNearestMarketR({}));
-        }
-      });
-  };
-
   useEffect(() => {
     setNearestMarket(selector.appState.nearestMarket);
   }, [selector.appState.nearestMarket]);
 
-  useEffect(() => {
-    if (Object.keys(selector.appState.selectedAddress).length !== 0) {
-      const { dlat: lat, dlng: lng } = selector.appState.selectedAddress;
-      getNearestMarket({ lat, lng });
-    } else {
-      getNearestMarketByDeviceAddress();
-    }
-  }, [selector.appState.selectedAddress]);
-
-  useEffect(() => {
-    checkUserData()
-      .then(userData => {
-        if (userData) {
-          dispatch(setAuthUserDataR(userData));
-        }
-      })
-      .catch(() => {});
-  }, []);
+  useInit();
 
   return (
     <Fragment>
