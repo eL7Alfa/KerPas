d6290fa746880c38efad4fa1d01210cfcb25fe7b
elalfa <ukasyahrz@outlook.com>
2021-11-12 16:14:43 +0800

    Add Header/Image, body(description)

--- src/components/Items/MarketDetails/index.tsx
+++ src/components/Items/MarketDetails/index.tsx
@@ -1,26 +1,65 @@
 import React, { useEffect, useState } from 'react';
-import { Modal } from '@mui/material';
+import { IconButton, Modal } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { setMarketDetailsModalR } from '../../../redux/actions/appRActions';
 import useStyles from './styles';
 import { rootReducerI } from '../../../redux/reducers';
+import Image from 'next/image';
+import { Close } from '@mui/icons-material';
 
 const MarketDetails = () => {
   const dispatch = useDispatch();
   const { appState } = useSelector((state: rootReducerI) => state);
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const initialData = {
+    address: '',
+    distance: { text: '', value: 0 },
+    location: '',
+    marketImg: '',
+    marketName: '',
+    description: '',
+  };
+  const [data, setData] = useState<{
+    marketImg: string;
+    marketName: string;
+    address: string;
+    distance: { text: string; value: number };
+    location: string;
+    description: string;
+  }>(initialData);
 
   const onClose = () => {
-    dispatch(setMarketDetailsModalR({ open: false }));
+    dispatch(setMarketDetailsModalR({ open: false, ...initialData }));
   };
 
   useEffect(() => {
     setOpen(appState.marketDetailsModal.open);
+    if (appState.marketDetailsModal.open) {
+      const { open, ...restData } = appState.marketDetailsModal;
+      setData(restData);
+    }
   }, [appState.marketDetailsModal.open]);
   return (
-    <Modal {...{ open, onClose }}>
-      <div />
+    <Modal {...{ open, onClose }} className={classes.root}>
+      <div className={classes.container}>
+        <div className={classes.header}>
+          <Image
+            alt={data.marketName}
+            src={data.marketImg}
+            layout={'fill'}
+            placeholder={'blur'}
+            blurDataURL={data.marketImg}
+          />
+        </div>
+        <div
+          className={classes.body}
+          dangerouslySetInnerHTML={{ __html: data.description }}
+        />
+        <IconButton className={classes.closeBtn} onClick={onClose}>
+          <Close />
+        </IconButton>
+      </div>
     </Modal>
   );
 };
--- src/components/Items/MarketDetails/styles.ts
+++ src/components/Items/MarketDetails/styles.ts
@@ -1,6 +1,40 @@
 import { createStyles, makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material/styles';
 
-const useStyles = makeStyles((theme: Theme) => createStyles({}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    container: {
+      position: 'relative',
+      width: `calc(100% - ${theme.spacing(2)})`,
+      [theme.breakpoints.up('sm')]: {
+        width: 600,
+      },
+      backgroundColor: '#ffffff',
+      borderRadius: theme.spacing(2),
+      overflow: 'hidden',
+    },
+    header: {
+      position: 'relative',
+      width: '100%',
+      height: 400,
+    },
+    body: {
+      display: 'flex',
+      textAlign: 'justify',
+      padding: theme.spacing(2),
+    },
+    closeBtn: {
+      position: 'absolute',
+      top: theme.spacing(1),
+      right: theme.spacing(1),
+    },
+  }),
+);
 
 export default useStyles;
