409fc5111e8f7fea5a2efc5752e3541094273198
elalfa <ukasyahrz@outlook.com>
2021-11-12 16:12:39 +0800

    Update types definition

--- src/redux/defaultStateR/appDefStateR.ts
+++ src/redux/defaultStateR/appDefStateR.ts
@@ -2,12 +2,28 @@ export type appDefStateRT = {
   selectedAddress: { [key: string]: any };
   nearestMarket: { [key: string]: any };
   myAddressesOpen: boolean;
-  marketDetailsModal: { open: boolean; [key: string]: any };
+  marketDetailsModal: {
+    open: boolean;
+    marketImg: string;
+    marketName: string;
+    address: string;
+    distance: { text: string; value: number };
+    location: string;
+    description: string;
+  };
 };
 
 export const appDefStateR: appDefStateRT = {
   selectedAddress: {},
   nearestMarket: {},
   myAddressesOpen: false,
-  marketDetailsModal: { open: false },
+  marketDetailsModal: {
+    open: false,
+    address: '',
+    distance: { text: '', value: 0 },
+    location: '',
+    marketImg: '',
+    marketName: '',
+    description: '',
+  },
 };
