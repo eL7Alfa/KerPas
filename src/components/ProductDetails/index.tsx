912202d430216b949bc244068230b7695e896e05
elalfa <ukasyahrz@outlook.com>
2021-11-18 00:23:21 +0800

    • Add Product Details; • Add Suppliers and Products sections that related to the category

--- src/components/ProductDetails/index.tsx
+++ src/components/ProductDetails/index.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
-import { ProductTypes } from '../constants';
+import React, { Fragment, useEffect, useState } from 'react';
+import { newProducts, ProductTypes } from '../constants';
 import useStyles from './styles';
+import Image from 'next/image';
+import { Button, ButtonBase, Divider, Paper, Typography } from '@mui/material';
+import { Store } from '@mui/icons-material';
+import SuppliersC from '../Items/Suppliers';
+import { useGetProducts, useGetSuppliers } from '../../Requests/GlobalRequests';
+import axios from '../../config/axios';
+import { supplierImgUrl } from '../../config/urls';
+import { useSelector } from 'react-redux';
+import { rootReducerI } from '../../redux/reducers';
+import { NearestMarketTypes } from '../../redux/defaultStateR/appDefStateR';
+import Products from '../Items/Products';
 
 export type ProductDetailsPropsTypes = {
   product: ProductTypes;
@@ -8,7 +19,195 @@ export type ProductDetailsPropsTypes = {
 
 const ProductDetails = ({ product }: ProductDetailsPropsTypes) => {
   const classes = useStyles();
-  return <div className={classes.root} />;
+  const { appState } = useSelector((state: rootReducerI) => state);
+  const [nearestMarket, setNearestMarket] = useState<NearestMarketTypes>({
+    calamat_toko: '',
+    cdeskripsi: '',
+    cfoto: '',
+    ckode_mitra: '',
+    ckota: '',
+    cnama_mitra: '',
+    distance: { text: '', value: 0 },
+    id: 0,
+  });
+  const [currentSuppliersPage, setCurrentSuppliersPage] = useState<number>(1);
+  const {
+    suppliers,
+    setSuppliers,
+    isSuppliersLoading,
+    lastSuppliersPage,
+    setIsSuppliersLoading,
+  } = useGetSuppliers({
+    marketCode: nearestMarket.ckode_mitra,
+    subCategory: product.classCode,
+  });
+  const [currentProductsPage, setCurrentProductsPage] = useState<number>(1);
+  const {
+    products,
+    setProducts,
+    lastProductsPage,
+    isProductsLoading,
+    setIsProductsLoading,
+  } = useGetProducts({
+    marketCode: nearestMarket.ckode_mitra,
+    productType: 'category',
+    categoryId: Number(product.classCode),
+  });
+
+  const onShowMoreSuppliersBtnClicked = () => {
+    const nextSuppliersPage = currentSuppliersPage + 1;
+    if (!isSuppliersLoading && nextSuppliersPage <= lastSuppliersPage) {
+      setIsSuppliersLoading(true);
+      axios()
+        .post(`/market/supplier?page=${nextSuppliersPage}`, {
+          limit: 12,
+          marketId: nearestMarket.ckode_mitra,
+        })
+        .then(({ data }) => {
+          const suppliers = data.map(
+            (d: {
+              cnama_supplier: any;
+              cimg_supplier: any;
+              calamat_supplier: any;
+            }) => {
+              return {
+                name: d.cnama_supplier,
+                imageUri: `${supplierImgUrl}/${d.cimg_supplier}`,
+                marketName: d.calamat_supplier,
+                block: 'A1 - B2',
+                location: 'Makassar',
+              };
+            },
+          );
+          setSuppliers(prevSuppliers => [...prevSuppliers, ...suppliers]);
+          setCurrentSuppliersPage(nextSuppliersPage);
+        })
+        .finally(() => setIsSuppliersLoading(false));
+    }
+  };
+
+  const onShowMoreProductBtnClicked = () => {
+    const nextProductPage = currentProductsPage + 1;
+    if (!isProductsLoading && nextProductPage <= lastProductsPage) {
+      setIsProductsLoading(true);
+      axios()
+        .post(`/market/product?page=${nextProductPage}`, {
+          limit: 12,
+          type: 'category',
+          marketId: nearestMarket.ckode_mitra,
+          subcategory: Number(product.classCode),
+        })
+        .then(({ data }) => {
+          setProducts(prevProducts => [
+            ...prevProducts,
+            ...newProducts(data.result.data),
+          ]);
+          setCurrentProductsPage(nextProductPage);
+        })
+        .finally(() => setIsProductsLoading(false));
+    }
+  };
+
+  useEffect(() => {
+    setNearestMarket(appState.nearestMarket);
+  }, [appState.nearestMarket]);
+
+  return (
+    <Fragment>
+      <div className={classes.detailsW}>
+        <div className={classes.sectionA}>
+          {product.imageUri && (
+            <div className={classes.imgC}>
+              <Image
+                src={product.imageUri}
+                placeholder={'blur'}
+                blurDataURL={product.imageUri}
+                alt={product.name}
+                layout={'fill'}
+                objectFit={'cover'}
+              />
+            </div>
+          )}
+        </div>
+        <div className={classes.sectionB}>
+          <Paper className={classes.sBContainer}>
+            <div className={classes.sBHeader}>
+              <Typography
+                variant={'h5'}
+                fontWeight={800}
+                className={classes.sBProductName}>
+                {product.name}
+              </Typography>
+              <ButtonBase className={classes.sBMarketNameW}>
+                <Store className={classes.sBMarketIcon} />
+                <Typography
+                  variant={'h6'}
+                  fontWeight={500}
+                  className={classes.sBMarketName}>
+                  {product.market.cnama_mitra}
+                </Typography>
+              </ButtonBase>
+            </div>
+            <Divider />
+            <div className={classes.sBBody}>
+              <Typography variant={'body1'} className={classes.sBSubtitle}>
+                Informasi Produk Pangan
+              </Typography>
+              <div className={classes.sBSubItem}>
+                <Typography variant={'body2'}>SKU</Typography>
+                <Typography variant={'body2'}>{product.sku}</Typography>
+              </div>
+              <div className={classes.sBSubItem}>
+                <Typography variant={'body2'}>Kategori</Typography>
+                <Typography variant={'body2'}>
+                  {`${product.category}/${product.class}`}
+                </Typography>
+              </div>
+              <div className={classes.sBSubItem}>
+                <Typography variant={'body2'}>Merk</Typography>
+                <Typography variant={'body2'}>{product.brand}</Typography>
+              </div>
+              <div className={classes.sBSubItem}>
+                <Typography variant={'body2'}>Berat</Typography>
+                <Typography
+                  variant={
+                    'body2'
+                  }>{`${product.weight} ${product.unit}`}</Typography>
+              </div>
+              <Typography variant={'body1'} className={classes.sBSubtitle}>
+                Deskripsi
+              </Typography>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: product.description!,
+                }}
+                className={classes.sBDescription}
+              />
+            </div>
+            <div className={classes.sBFooter}>
+              <Button variant={'contained'} className={classes.sBAddToCartBtn}>
+                TAMBAH KE KERANJANG
+              </Button>
+            </div>
+          </Paper>
+        </div>
+      </div>
+      <SuppliersC
+        name={`Kios Pangan ${product.class}`}
+        data={suppliers}
+        onShowMoreBtnClicked={onShowMoreSuppliersBtnClicked}
+        isLoading={isSuppliersLoading}
+        isLastPageReached={currentSuppliersPage + 1 > lastSuppliersPage}
+      />
+      <Products
+        name={`${product.class} Lainnya`}
+        data={products}
+        onShowMoreBtnClicked={onShowMoreProductBtnClicked}
+        isLoading={isProductsLoading}
+        isLastProductReached={currentProductsPage + 1 > lastProductsPage}
+      />
+    </Fragment>
+  );
 };
 
 export default ProductDetails;
--- src/components/ProductDetails/styles.ts
+++ src/components/ProductDetails/styles.ts
@@ -3,9 +3,103 @@ import { Theme } from '@mui/material/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
+    detailsW: {
       display: 'flex',
       flexDirection: 'row',
+      justifyContent: 'center',
+      backgroundColor: 'rgba(0,0,0,0.1)',
+      padding: theme.spacing(4),
+      borderRadius: theme.spacing(2),
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+    sectionA: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+    },
+    sectionB: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    imgC: {
+      position: 'relative',
+      width: 400,
+      height: 400,
+      overflow: 'hidden',
+      borderTopLeftRadius: 8,
+      borderBottomLeftRadius: 8,
+      boxShadow: '0 20px 15px -10px rgba(0,0,0,0.15)',
+    },
+    sBContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      boxShadow: '0 40px 25px -20px rgba(0,0,0,0.15)',
+      padding: theme.spacing(2),
+      minWidth: 500,
+      borderRadius: theme.spacing(2),
+      minHeight: 550,
+    },
+    sBHeader: {
+      padding: `${theme.spacing(1)} 0`,
+    },
+    sBProductName: {
+      color: '#424242',
+    },
+    sBMarketNameW: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    sBMarketIcon: {
+      color: theme.palette.primary.main,
+      fontSize: 30,
+    },
+    sBMarketName: {
+      color: '#424242',
+    },
+    sBBody: {
+      padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
+      maxHeight: 400,
+      overflow: 'auto',
+    },
+    sBSubtitle: {
+      fontWeight: 800,
+      colo4: '#424242',
+      marginTop: theme.spacing(2),
+    },
+    sBSubItem: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+    },
+    sBDescription: {
+      fontSize: theme.typography.body2.fontSize,
+      fontWeight: 'normal',
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+    },
+    sBFooter: {
+      display: 'flex',
+      marginTop: 'auto',
+      paddingTop: theme.spacing(2),
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    sBAddToCartBtn: {
+      width: '100%',
+      backgroundColor: theme.palette.primary.main,
+      borderRadius: theme.spacing(1),
+      boxShadow: '0 5px 5px -2px rgba(0,0,0,0.15)',
+      '&:hover': {
+        backgroundColor: theme.palette.primary.main,
+        boxShadow: '0 10px 15px -5px rgba(0,0,0,0.3)',
+      },
     },
   }),
 );
