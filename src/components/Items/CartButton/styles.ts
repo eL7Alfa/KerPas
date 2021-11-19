74f4592a5a5bce28ffec26d84b6d630ac7a55d09
elalfa <ukasyahrz@outlook.com>
2021-11-19 17:38:15 +0800

    • Update CartButton styles

--- src/components/Items/CartButton/styles.ts
+++ src/components/Items/CartButton/styles.ts
@@ -24,10 +24,41 @@ const useStyles = makeStyles((theme: Theme) =>
       paddingTop: 10,
     },
     paperPopCartList: {
+      minWidth: theme.breakpoints.values.xs,
       padding: '10px 0',
       borderRadius: 10,
       boxShadow:
         '0 13px 27px -5px rgb(50 50 93 / 25%), 0 8px 16px -8px rgb(0 0 0 / 30%), 0 -6px 16px -6px rgb(0 0 0 / 3%)',
+    },
+    cMPHeader: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
+    },
+    cMPHTitleW: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    cMPHTitleIcon: {
+      color: theme.palette.primary.main,
+    },
+    cMPHTitle: {
+      color: '#424242',
+      fontWeight: 800,
+      padding: `0 ${theme.spacing(1)}`,
+    },
+    showAllItemBtn: {
+      color: theme.palette.primary.main,
+      fontWeight: 800,
+      padding: `${theme.spacing(0.5)} ${theme.spacing(1)}`,
+      fontSize: 16,
+      borderRadius: theme.spacing(1),
+    },
+    cMPItemName: {
+      color: '#424242',
+      fontWeight: 800,
     },
   }),
 );
