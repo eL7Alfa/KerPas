ac1a747c3ad203c713ab61e5d6a16661dbc93c9b
elalfa <ukasyahrz@outlook.com>
2021-11-20 17:54:56 +0800

    • Simplify condition; • Remove unused params

--- src/components/Items/SelectSupplier/SelectAbleSupplier/index.tsx
+++ src/components/Items/SelectSupplier/SelectAbleSupplier/index.tsx
@@ -2,7 +2,6 @@ import useStyles from './styles';
 import { Button, Paper } from '@mui/material';
 import Image from 'next/image';
 import { CheckCircleRounded } from '@mui/icons-material';
-import { Fragment } from 'react';
 
 export type SupplierPropsTypes = {
   active: boolean;
@@ -18,12 +17,10 @@ export type SupplierPropsTypes = {
 
 const SelectAbleSupplier = ({
   active = false,
-  supplierId,
   imageUri,
   name,
   marketName,
   block,
-  location,
   onClick = () => {},
 }: SupplierPropsTypes) => {
   const classes = useStyles();
@@ -45,18 +42,10 @@ const SelectAbleSupplier = ({
             <div className={classes.name}>{name}</div>
             <div className={classes.marketName}>{marketName}</div>
             <div className={classes.block}>{block}</div>
-            {/*<div className={classes.location}>*/}
-            {/*  <Room className={classes.locationIcon} />*/}
-            {/*  {location}*/}
-            {/*</div>*/}
           </div>
         </Paper>
       </Button>
-      {active ? (
-        <CheckCircleRounded className={classes.activeIcon} />
-      ) : (
-        <Fragment />
-      )}
+      {active && <CheckCircleRounded className={classes.activeIcon} />}
     </div>
   );
 };
