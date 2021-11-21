7fe269da9a25f603fd6c17737752773d6b2e0caa
elalfa <ukasyahrz@outlook.com>
2021-11-22 04:43:58 +0800

    • Add new module mathRandomRange

---
+++ src/modules/mathRandomRange/index.ts
@@ -0,0 +1,6 @@
+const mathRandomRange = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+export default mathRandomRange;
