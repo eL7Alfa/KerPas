b901aa1f278731613e484f79ec14762cda5bedcb
eL7Alfa <ukasyahrz@outlook.com>
2021-11-24 05:08:42 +0800

    Open Auth modal if no userData

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../../config/axios';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { rootReducerI } from '../../redux/reducers';
 import useStyles from './styles';
 import {
@@ -14,9 +14,11 @@ import {
 import toRupiah from '../../modules/toRupiah';
 import Image from 'next/image';
 import { productImgUrl } from '../../config/urls';
+import { setAuthModalOpenR } from '../../redux/actions/authRActions';
 
 const Transactions = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const { authState } = useSelector((state: rootReducerI) => state);
   const [transactions, setTransactions] = useState([]);
   const [statusCode, setStatusCode] = useState('0');
@@ -54,6 +56,9 @@ const Transactions = () => {
 
   useEffect(() => {
     getTransaction();
+    if (!authState.userData.id) {
+      dispatch(setAuthModalOpenR(true));
+    }
   }, [authState.userData]);
 
   return (
